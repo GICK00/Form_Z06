46323c4fb828b44708681c1ca2d8e86fdf8bcc2e
Gick <71695624+GICK00@users.noreply.github.com>
2021-12-07 13:27:22 +0300

    Add files via upload

---
+++ Form_Z06_4/Form_Z06_4/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Form_Z06_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxArr = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Нахождение положительного \r\nчисла";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(169, 62);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(76, 20);
+            this.textBoxN.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите размер N \r\nдвумерного массива.";
+            // 
+            // textBoxArr
+            // 
+            this.textBoxArr.Location = new System.Drawing.Point(42, 88);
+            this.textBoxArr.Multiline = true;
+            this.textBoxArr.Name = "textBoxArr";
+            this.textBoxArr.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxArr.Size = new System.Drawing.Size(203, 131);
+            this.textBoxArr.TabIndex = 3;
+            this.textBoxArr.Text = "Введите элементы массива через \",\"";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(105, 225);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Утин Д. С.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 281);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxArr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form Z06.4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxArr;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form_Z06_4/Form_Z06_4/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Form_Z06_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(textBoxN.Text);
+                if (n < 1)
+                    throw new Exception("Размер массива не может быть меньше 1!");
+                int[,] mas = new int[n, n];
+                int[] mas2 = new int[n];
+
+                read(mas, n);
+                print(mas, n);
+                plus(mas, mas2, n);
+                print(mas2, n);
+            }
+            catch (FormatException)
+            {
+                textBoxArr.Text = "Некорректный ввод данных!";
+            }
+            catch (Exception ex)
+            {
+                textBoxArr.Text = $"{ex.Message}";
+            }
+        }
+        private void read(int[,] dvymer, int n)
+        {
+            int[] mas = textBoxArr.Text.Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+            if (n * n != mas.Length)
+                throw new Exception("Количество элементов не соответствует размеру массива!");
+            int w = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dvymer[i, j] = mas[w];
+                    w++;
+                }
+            }
+            textBoxArr.Text = "";
+        }
+        private void print(int[,] dvymer, int n)
+        {
+            textBoxArr.Text += "Размер массива: " + n + ", " + n + "\r\nЭлементы:\r\n";
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    textBoxArr.Text += String.Format("{0,4}", dvymer[i, j]);
+                }
+                textBoxArr.Text += "\r\n";
+            }
+        }
+        public void print(int[] mas2, int n)
+        {
+            textBoxArr.Text += "Размер массива: " + n + "\r\nЭлементы:\r\n";
+            for (int i = 0; i < n; i++)
+            {
+                textBoxArr.Text += "  " + mas2[i];
+            }
+            textBoxArr.Text += "\r\n";
+        }
+        static void plus(int[,] mas, int[] mas2, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int plus = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (mas[j, i] >= plus)
+                    {
+                        plus = mas[j, i];
+                        break;
+                    }
+                }
+                mas2[i] = plus;
+            }
+        }
+    }
+}
